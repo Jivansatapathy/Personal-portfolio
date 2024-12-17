784a41fab78c56914ea20369adbf37804a3dbace
Jivansatapathy <98332726+Jivansatapathy@users.noreply.github.com>
2024-12-17 20:13:17 +0000

    Fifth Commit

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -10,7 +10,6 @@ export const Banner = () => {
   const [isDeleting, setIsDeleting] = useState(false);
   const [text, setText] = useState('');
   const [delta, setDelta] = useState(300 - Math.random() * 100);
-  const [index, setIndex] = useState(1);
   const toRotate = [ "Web Developer", "Content Writer", "Python Developer" ];
   const period = 2000;
 
